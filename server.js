81a4c6fb2b014b003c38aeba56f33480db09fcac
Tyiss Goodman <sticktyiss@gmail.com>
2023-04-05 14:09:31 -0700

    Enabled connectivity

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
-app.listen(5000,
-  () => console.log(`server running on 5000`)
+app.listen(4000,
+  () => console.log(`server running on 4000`)
 )
