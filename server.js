56797779678fcd3d4d5623b85e51b0b79d0623c4
Tyiss Goodman <sticktyiss@gmail.com>
2023-04-07 15:55:10 -0700

    Fixed filepaths and endpoints

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
 app.use(express.static(`${__dirname}/public`))
-app.use(cors())
 
 const { incCount, getCounts } = require('./controller')
 
